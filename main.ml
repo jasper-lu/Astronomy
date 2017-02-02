dfa6aaff38b01ca837d6e9b74a465cb4569bdc7a
Jasper Lu <thejasperlu@gmail.com>
2017-02-02 02:57:14 -0600

    complete

--- main.ml
+++ main.ml
@@ -10,13 +10,33 @@ A.close part1;;
 
 let part2 = A.init ["Cairo";"PNG";"G_RToTime.png"];;
 A.Axes.box part2;
+A.Viewport.xlabel part2 "Time (Gigayears)";
+A.Viewport.ylabel part2 "g-r";
 A.fx part2 Util.g_r_at_time 0. 10.;
 A.close part2;;
 
-let mag_to_g_r (t : float) = (Util.g_r_at_time t, Util.stellar_mag_at_time t) in 
+let mag_to_gr (t : float) = (Util.g_r_at_time t, Util.stellar_mag_at_time t);;
+
+let t_list = [0.01;0.1;1.;2.;5.;10.] in
+let t_arr = Array.of_list t_list in
+let t_label = ["10 Myr"; "100 Myr"; "1 Gyr"; "2 Gyr"; "5 Gyr"; "10 Gyr"] in 
+let t_label_arr = Array.of_list t_label in
+let gr = Array.make 6 0. in
+let mag = Array.make 6 0. in
+
+for i = 0 to 5 do 
+    gr.(i) <- Util.g_r_at_time (t_arr.(i));
+    mag.(i) <- Util.stellar_mag_at_time (t_arr.(i));
+done;
 
 let part3 = A.init ["Cairo";"PNG";"MagToG_R.png"] in 
 A.Axes.box part3; 
-(*A.Array.xy ~style: `Lines part3 g_r mag;*)
-A.xyf ~n:2000 part3 mag_to_g_r 0. 10.;
+A.Viewport.xlabel part3 "g-r";
+A.Viewport.ylabel part3 "M(t) - M(0)";
+A.xyf ~n:2000 part3 mag_to_gr 0. 10.;
+A.Array.xy part3 gr mag;
+let label = A.Backend.RB in
+for i = 0 to 5 do 
+    A.Viewport.text part3 (gr.(i)) (mag.(i)) (t_label_arr.(i)) ~pos: label;
+done;
 A.close part3;;
--- util.ml
+++ util.ml
@@ -2,6 +2,18 @@
 let our_min_mass = 0.08;;
 let our_max_mass = 50.;; 
 
+(* UTILS *)
+
+let integrate f a b steps =
+    let meth f x h = f (x +. h /. 2.) in 
+    let h = (b -. a) /. float_of_int steps in
+    let rec helper i s =
+        if i >= steps then s
+        else helper (succ i) (s +. meth f (a +. h *. float_of_int i) h)
+    in
+    h *. helper 0 0.
+;;
+
 (* Now create IMF derivatives 
  * These have the exp at the front to mimic if it were found through integration *)
 let power_diff low high exp = (1. /. exp) *. (high ** exp -. low ** exp);;
@@ -34,20 +46,7 @@ let abs_mag_diff (lum : float) : float =
 let stellar_mag_at_time (t : float) : float = 
     abs_mag_diff (stellar_luminosity t);;
 
-
-(* UTILS *)
-
-let integrate f a b steps =
-    let meth f x h = f (x +. h /. 2.) in 
-    let h = (b -. a) /. float_of_int steps in
-    let rec helper i s =
-        if i >= steps then s
-        else helper (succ i) (s +. meth f (a +. h *. float_of_int i) h)
-    in
-    h *. helper 0 0.
-;;
-
-(* This is the for part 2 *)
+(* This is for part 2 *)
 let gr (m : float) : float = log ((m +. 2.) /. m) -. 0.65;;
 
 let lum_ratio (m : float) : float = 
@@ -57,16 +56,20 @@ let lum_ratio (m : float) : float =
     lr *. lg
 ;;
 
+(* This basically gets us our L_g *)
 let lum_integrated min max = integrate lum_ratio min max 1000;;
 
+(* This returns the g-r of a galaxy with mass between min and max *)
 let gr_galaxy (min : float) (max : float) = 
     let weight = 3.5 in 
+    (* This basically gets us our L_r *)
     let diff = integrate (fun x -> x ** 3.5) min max 1000 in
     (*let diff = max -. min in *)
     let lum_ratio_avg = (lum_integrated min max) /. diff in 
     -.2.5 *. (log10 lum_ratio_avg)    
 ;;
 
+(* This returns the g-r for a galaxy at time t after formation *)
 let g_r_at_time (t: float) : float = 
     gr_galaxy our_min_mass (mass_death_at_time t)
 ;;
